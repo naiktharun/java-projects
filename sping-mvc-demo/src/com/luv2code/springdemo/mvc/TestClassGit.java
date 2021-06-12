23aa7dd6c0700c0cdc5a24b6a640e4939c5b1135
tharun1234 <naiktharun@gmail.com>
2021-06-12 15:07:59 +0530

    Added welcome

--- sping-mvc-demo/src/com/luv2code/springdemo/mvc/TestClassGit.java
+++ sping-mvc-demo/src/com/luv2code/springdemo/mvc/TestClassGit.java
@@ -4,6 +4,9 @@ public class TestClassGit {
 
   public static void main(String[] args) {
 	
+	  
+	  //identation added 
 	  System.out.println("hello git");
+	  System.out.println("welcome");
 }
 }
