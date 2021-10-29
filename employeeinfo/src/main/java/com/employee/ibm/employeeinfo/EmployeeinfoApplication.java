54e143a7f0c6ac47332e26e6e190e9fe3e3db6c3
tharun1234 <naiktharun@gmail.com>
2021-10-29 18:49:52 +0530

    new project to check connection

---
+++ employeeinfo/src/main/java/com/employee/ibm/employeeinfo/EmployeeinfoApplication.java
@@ -0,0 +1,13 @@
+package com.employee.ibm.employeeinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeinfoApplication.class, args);
+	}
+
+}
